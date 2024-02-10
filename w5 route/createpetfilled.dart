c0797e4124ee2b9c415c7c37d7f60a0c46b1d32c
Supithcha Jongphoemwatthanaphon <106173639+supithcha@users.noreply.github.com>
2024-02-10 14:03:47 +0700

    Update createpetfilled.dart

--- w5 route/createpetfilled.dart
+++ w5 route/createpetfilled.dart
@@ -13,13 +13,12 @@ class petprofilefilled extends StatefulWidget {
   const petprofilefilled({Key? key, required this.petInfo, this.image})
       : super(key: key);
 
-
   @override
-   _petprofilefilledState createState() => _petprofilefilledState();
+  _petprofilefilledState createState() => _petprofilefilledState();
 }
 
 class _petprofilefilledState extends State<petprofilefilled> {
-   int _selectedIndex = 0;
+  int _selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -36,93 +35,48 @@ class _petprofilefilledState extends State<petprofilefilled> {
                 // Photo Section
                 SizedBox(height: 30),
                 Container(
-  height: 170,
-  width: double.infinity,
-  decoration: BoxDecoration(
-    border: Border.all(),
-    borderRadius: BorderRadius.circular(10),
-    boxShadow: [
-      BoxShadow(
-        color: Colors.grey.shade400,
-        offset: const Offset(
-          3.0,
-          3.0,
-        ),
-        blurRadius: 5.0,
-        spreadRadius: 1.0,
-      ),
-      BoxShadow(
-        color: Colors.white,
-        offset: const Offset(0.0, 0.0),
-        blurRadius: 0.0,
-        spreadRadius: 0.0,
-      ),
-    ],
-  ),
-  child: Stack(
-    alignment: Alignment.bottomRight,
-    children: [
-      if (widget.image != null)
-              Image.memory(
-                widget.image!,
-                width: double.infinity,
-                height: double.infinity,
-                fit: BoxFit.cover,
-              ),
-      IconButton(
-        icon: Icon(Icons.camera_alt),
-        onPressed: () {
-          // Handle camera icon click
-        },
-      ),
-    ],
-  ),
-),
-
-                // Container(
-                //   height: 170,
-                //   width: 550,
-                //   // width: double.infinity,
-                //   decoration: BoxDecoration(
-                //     border: Border.all(),
-                //     borderRadius: BorderRadius.circular(10),
-                //     boxShadow: [
-                //       // BoxShadow(color: Colors.white, spreadRadius: 0),
-                //       BoxShadow(
-                //         color: Colors.grey.shade400,
-                //         offset: const Offset(
-                //           3.0,
-                //           3.0,
-                //         ),
-                //         blurRadius: 5.0,
-                //         spreadRadius: 1.0,
-                //       ), //BoxShadow
-                //       BoxShadow(
-                //         color: Colors.white,
-                //         offset: const Offset(0.0, 0.0),
-                //         blurRadius: 0.0,
-                //         spreadRadius: 0.0,
-                //       ), //BoxShadow
-                //     ],
-                //   ),
-                //   child: Stack(
-                //     alignment: Alignment.bottomRight,
-                //     children: [
-                //       Center(
-                //         child: Image.asset(
-                //           'assets/chanel.png', 
-                //           fit: BoxFit.fill,
-                //         ),
-                //       ),
-                //       IconButton(
-                //         icon: Icon(Icons.camera_alt),
-                //         onPressed: () {
-                //           // Handle camera icon click
-                //         },
-                //       ),
-                //     ],
-                //   ),
-                // ),
+                  height: 170,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    border: Border.all(),
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.shade400,
+                        offset: const Offset(
+                          3.0,
+                          3.0,
+                        ),
+                        blurRadius: 5.0,
+                        spreadRadius: 1.0,
+                      ),
+                      BoxShadow(
+                        color: Colors.white,
+                        offset: const Offset(0.0, 0.0),
+                        blurRadius: 0.0,
+                        spreadRadius: 0.0,
+                      ),
+                    ],
+                  ),
+                  child: Stack(
+                    alignment: Alignment.bottomRight,
+                    children: [
+                      if (widget.image != null)
+                        Image.memory(
+                          widget.image!,
+                          width: double.infinity,
+                          height: double.infinity,
+                          fit: BoxFit.cover,
+                        ),
+                      IconButton(
+                        icon: Icon(Icons.camera_alt),
+                        onPressed: () {
+                          // Handle camera icon click
+                        },
+                      ),
+                    ],
+                  ),
+                ),
                 SizedBox(height: 30),
                 // Name and Gender Section
                 Row(
@@ -136,9 +90,6 @@ class _petprofilefilledState extends State<petprofilefilled> {
                             children: [
                               Text(
                                 'Name',
-                                // style: TextStyle(
-                                //   fontWeight: FontWeight.bold,
-                                // ),
                               ),
                               SizedBox(height: 10),
                               TextFormField(
@@ -160,9 +111,7 @@ class _petprofilefilledState extends State<petprofilefilled> {
                             child: IconButton(
                               icon: Icon(
                                   Icons.drive_file_rename_outline_outlined),
-                              onPressed: () {
-                                // Add your edit icon onPressed logic here
-                              },
+                              onPressed: () {},
                             ),
                           ),
                         ],
@@ -177,9 +126,6 @@ class _petprofilefilledState extends State<petprofilefilled> {
                             children: [
                               Text(
                                 'Gender',
-                                // style: TextStyle(
-                                //   fontWeight: FontWeight.bold,
-                                // ),
                               ),
                               SizedBox(height: 10),
                               TextFormField(
@@ -225,9 +171,6 @@ class _petprofilefilledState extends State<petprofilefilled> {
                             children: [
                               Text(
                                 'Breed or Mix',
-                                // style: TextStyle(
-                                //   fontWeight: FontWeight.bold,
-                                // ),
                               ),
                               SizedBox(height: 10),
                               TextFormField(
@@ -266,9 +209,6 @@ class _petprofilefilledState extends State<petprofilefilled> {
                             children: [
                               Text(
                                 'Date of Birth',
-                                // style: TextStyle(
-                                //   fontWeight: FontWeight.bold,
-                                // ),
                               ),
                               SizedBox(height: 10),
                               TextFormField(
@@ -312,9 +252,6 @@ class _petprofilefilledState extends State<petprofilefilled> {
                             children: [
                               Text(
                                 'Weight',
-                                // style: TextStyle(
-                                //   fontWeight: FontWeight.bold,
-                                // ),
                               ),
                               SizedBox(height: 10),
                               TextFormField(
@@ -353,9 +290,6 @@ class _petprofilefilledState extends State<petprofilefilled> {
                             children: [
                               Text(
                                 'Pet type',
-                                // style: TextStyle(
-                                //   fontWeight: FontWeight.bold,
-                                // ),
                               ),
                               SizedBox(height: 10),
                               TextFormField(
